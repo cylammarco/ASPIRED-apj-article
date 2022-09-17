2840ebc593e8f665f2be288011e6b8a8a126f55d
cylammarco <cylammarco@gmail.com>
2022-09-17 05:54:07 +0300

    updated plots
    Former-commit-id: 908b1ccb60b9e55b09e237ab0dc4f5051a6deaf8

--- fig_09_use_case_plot.py
+++ fig_09_use_case_plot.py
@@ -5,6 +5,50 @@ import numpy as np
 
 plt.ion()
 
+# ISIS
+isis_blue = fits.open('wht-isis-ucwd-pso1801p6254/pso_reduced_blue_science_0.fits')[1]
+isis_red = fits.open('wht-isis-ucwd-pso1801p6254/pso_reduced_red_science_0.fits')[1]
+
+isis_wave_blue = np.linspace(
+    isis_blue.header["CRVAL1"],
+    isis_blue.header["CRVAL1"]
+    + isis_blue.header["CDELT1"] * isis_blue.header["NAXIS1"],
+    isis_blue.header["NAXIS1"],
+)
+isis_wave_red = np.linspace(
+    isis_red.header["CRVAL1"],
+    isis_red.header["CRVAL1"]
+    + isis_red.header["CDELT1"] * isis_red.header["NAXIS1"],
+    isis_red.header["NAXIS1"],
+)
+
+isis_flux_blue = isis_blue.data
+isis_flux_red = isis_red.data
+
+isis_wave_blue_resampled = isis_wave_blue[::15]
+isis_wave_red_resampled = isis_wave_red[::15]
+isis_flux_blue_resampled = spectres(isis_wave_blue_resampled, isis_wave_blue, isis_flux_blue)
+isis_flux_red_resampled = spectres(isis_wave_red_resampled, isis_wave_red, isis_flux_red)
+
+
+
+# ACAM
+acam = fits.open('wht-acam-ucwd-pso1801p6254/pso_reduced_science_0.fits')[1]
+
+acam_wave = np.linspace(
+    acam.header["CRVAL1"],
+    acam.header["CRVAL1"]
+    + acam.header["CDELT1"] * acam.header["NAXIS1"],
+    acam.header["NAXIS1"],
+)
+
+acam_flux = acam.data
+
+acam_wave_resampled = acam_wave[::10]
+acam_flux_resampled = spectres(acam_wave_resampled, acam_wave, acam_flux)
+
+
+
 # DOLORES dmwd
 dolores_fits_1 = fits.open("tng-dolores-dmwd/dmwd_blue_1_science_1.fits")[1]
 dolores_fits_2 = fits.open("tng-dolores-dmwd/dmwd_blue_2_science_1.fits")[1]
@@ -121,8 +165,9 @@ ymax = np.nanmax((r2500u_1_flux, r2500u_2_flux))
 plt.rcParams.update({"font.size": 16})
 plt.rcParams.update({"legend.fontsize": 12})
 
-plt.figure(1, figsize=(8, 10))
+plt.figure(1, figsize=(8, 8))
 plt.clf()
+
 # OSIRIS
 plt.plot(
     r2500u_1_wave,
@@ -142,6 +187,7 @@ plt.plot(
     alpha=0.6,
     label="GTC/OSIRIS R1000B",
 )
+plt.text(4400, 6.1e-14, "Many blue absorption lines", color="royalblue", alpha=0.8)
 
 # DOLORES
 plt.plot(
@@ -155,15 +201,38 @@ plt.plot(
     dolores_dm_flux + 1.5e-14,
     color="crimson",
     alpha=0.6,
-    label="TNG/DOLORES dM",
+    label="TNG/DOLORES LR-B",
 )
-plt.text(3750, 2.3e-14, "Simultaneous Extraction", color="crimson", alpha=0.8)
+plt.text(3650, 2.3e-14, "Simultaneous Extraction", color="crimson", alpha=0.8)
 
-plt.xlim(3500, 8000)
+# ISIS
+plt.plot(
+    isis_wave_blue_resampled[(isis_wave_blue_resampled>4250) & (isis_wave_blue_resampled<5350)],
+    isis_flux_blue_resampled[(isis_wave_blue_resampled>4250) & (isis_wave_blue_resampled<5350)] * 500.,
+    color="olivedrab",
+    label="WHT/ISIS R300B",
+)
+plt.plot(
+    isis_wave_red_resampled[(isis_wave_red_resampled>7250) & (isis_wave_red_resampled<9000)],
+    isis_flux_red_resampled[(isis_wave_red_resampled>7250) & (isis_wave_red_resampled<9000)] * 500.,
+    color="olivedrab",
+    label="WHT/ISIS R300R",
+    alpha=0.6,
+)
+
+# ACAM
+plt.plot(
+    acam_wave_resampled[acam_wave_resampled>5150], acam_flux_resampled[acam_wave_resampled>5150] * 1000.,
+    color='orange',
+    label="WHT/ACAM V400"
+)
+plt.text(6100, 0.66e-14, "Very low SNR", color="olivedrab", alpha=0.8)
+
+plt.xlim(3500, 9000)
 plt.ylim(0.0, 7e-14)
-plt.xlabel(r"Wavelength / $\mathrm{\AA}$")
-plt.ylabel(r"Flux / ( erg / cm / cm / s / $\mathrm{\AA}$)")
-plt.grid()
+ax.set_yticks([])
+plt.xlabel(r"Wavelength ($\mathrm{\AA}$)")
+plt.ylabel("Arbitrary Flux (per $\mathrm{\AA}$)")
 plt.legend()
 plt.tight_layout()
 plt.savefig("fig_09_use_case_plots.pdf")
